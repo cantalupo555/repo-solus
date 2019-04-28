12419ce15afd053a52cd13fd20b7e7878c4794ac
cantalupo <cantalupo@protonmail.com>
2019-04-28 00:15:34 -0300

    Update

--- party/office/xmind-zen/actions.py
+++ party/office/xmind-zen/actions.py
@@ -7,11 +7,9 @@ from pisi.actionsapi import get, pisitools, shelltools
 NoStrip = ["/opt", "/usr"]
 IgnoreAutodep = True
 
-Version = get.srcVERSION()
-
 def setup():
 	shelltools.system("pwd")
-	shelltools.system("ar xf XMind-ZEN-for-Linux-64bit.deb" % (get.srcVERSION()))
+	shelltools.system("ar xf XMind-ZEN-for-Linux-64bit.deb")
 	shelltools.system("tar xf data.tar.xz")
 
 def install():
